fb046477d14baf9b7e2de1b37dd3598b75cfdab6
Mostafa_Mahmoud <157222523+Mostafa2115@users.noreply.github.com>
2024-12-07 22:43:05 +0200

    Problem B

---
+++ Rookies/Task3/Books.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long n;
+    long long t;
+    cin >> n >> t;
+    vector<long> a(n);
+    for (long i = 0; i < n; i++)
+        cin >> a[i];
+
+    long x = 0, sum = 0, y = 0;
+    for (long i = 0; i < n; i++)
+    {
+        sum += a[i];
+        while (sum > t)
+        {
+            sum -= a[y];
+            y++;
+        }
+        x = max(x, i - y + 1);
+    }
+    cout << x;
+    return 0;
+}
